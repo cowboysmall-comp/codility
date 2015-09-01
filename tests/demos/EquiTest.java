7bfd67898fffcca3db759375a991fc50c499ccfc
Jerry Kiely <jerry_kiely@mastercard.com>
2015-09-01 11:29:37 +0100

    added gradle build + gradle wrapper support

--- tests/demos/EquiTest.java
+++ tests/demos/EquiTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class EquiTest {
 
     @Test
-    public void testSomething() {
+    public void testSolution() {
 
         Equi equi = new Equi();
         assertEquals(1, equi.solution(new int[]{-1, 3, -4, 5, 1, -6, 2, 1}));
